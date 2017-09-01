cb04d1d33b8d244a9fbf6338b153488613b1d4c4
Eduardo Schettino <schettino72@gmail.com>
2017-09-01 16:54:36 +0400

    remove unit-test for sample. sample is already being tested on tes_cmd.

--- tests/test_sqla_yaml_fixtures.py
+++ tests/test_sqla_yaml_fixtures.py
@@ -444,37 +444,3 @@ def test_mapper_must_contain_list(session):
     assert 'must contain a sequence(list)' in str(exc_info)
     assert 'User' in str(exc_info)
 
-
-def test_doc_sample(session):
-    # test the example used in the docs
-    fixture = """
-- User:
-  - __key__: joey
-    username: joey
-    email: joey@example.com
-    profile:
-      name: Jeffrey
-
-  - __key__: dee
-    username: deedee
-    email: deedee@example.com
-
-- Profile:
-  - user: dee
-    name: Douglas
-
-- Group:
-  - name: Ramones
-    members: [joey.profile, dee.profile]
-"""
-    sqla_yaml_fixtures.load(BaseModel, session, fixture)
-    groups = session.query(Group).all()
-    assert len(groups) == 1
-    assert groups[0].name == 'Ramones'
-    users = session.query(User).all()
-    assert len(users) == 2
-    assert users[0].profile.groups[0].group.name == 'Ramones'
-    assert users[0].username == 'joey'
-    assert users[0].profile.name == 'Jeffrey'
-
-
