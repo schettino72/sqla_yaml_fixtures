4c469ce679dc4692557669130e9e52763a6d4745
Eduardo Schettino <schettino72@gmail.com>
2024-09-30 03:40:26 +0700

    initial support for core (non-ORM) SQLAlchemy.

--- sqla_yaml_fixtures/__init__.py
+++ sqla_yaml_fixtures/__init__.py
@@ -5,18 +5,7 @@ import sqlalchemy
 from sqlalchemy.orm.relationships import RelationshipProperty
 
 
-__version__ = (1, 1, 0)
-
-
-def from_registry(Base, model_name):
-    """Compatibility for SQLAlchemy 1.4 changes
-
-    https://github.com/sqlalchemy/sqlalchemy/commit/450f5c0d6519a439f4025c3892fe4cf3ee2d892c
-    """
-    if hasattr(Base, '_decl_class_registry'): # SQLAlchemy < 1.4
-        return Base._decl_class_registry[model_name]
-    else:
-        return Base.registry._class_registry[model_name]
+__version__ = (1, 2, 'dev0')
 
 
 class Store:
@@ -39,6 +28,21 @@ class Store:
     def put(self, key, value):
         assert key not in self._store, "Duplicate key:{}".format(key)
         self._store[key] = value
+
+
+#############################
+### ORM
+
+def from_registry(Base, model_name):
+    """Compatibility for SQLAlchemy 1.4 changes
+
+    https://github.com/sqlalchemy/sqlalchemy/commit/450f5c0d6519a439f4025c3892fe4cf3ee2d892c
+    """
+    if hasattr(Base, '_decl_class_registry'): # SQLAlchemy < 1.4
+        return Base._decl_class_registry[model_name]
+    else:
+        return Base.registry._class_registry[model_name]
+
 
 
 @lru_cache()
@@ -222,4 +226,94 @@ def load(ModelBase, session, fixture_text, loader=None):
                               model_name, creator, key, fields)
             session.add(obj)
     session.commit()
+    return store
+
+
+
+#############################
+### Core SQLAlchemy
+
+
+async def _insert_row(tables, connection, store, table_name, key, values):
+    """
+    Create and insert a row into the given table from the provided values.
+
+    :param tables: Dictionary of SQLAlchemy Core tables (replaces models).
+    :param connection: SQLAlchemy connection object for executing queries.
+    :param store: Store of previously created objects.
+    :param table_name: Name of the table (string).
+    :param key: Optional key for the object in the store.
+    :param values: Column values for the row to be inserted.
+    """
+    # Get reference to the SQLAlchemy Core table
+    table = tables[table_name]
+
+    # Resolve references in values (i.e., nested objects)
+    resolved_values = {}
+    for name, value in values.items():
+        # print(name, value)
+        col = table.c.get(name)
+        if col.foreign_keys:
+            assert len(col.foreign_keys) == 1
+            fk = next(iter(col.foreign_keys))
+            fk_col = fk.target_fullname.split('.')[-1]
+            obj = store.get(value)
+            resolved_values[name] = getattr(obj, fk_col)
+        else:
+            resolved_values[name] = value
+
+    # Execute insert statement
+    insert_stmt = table.insert().values(**resolved_values).returning(table)
+    result = await connection.execute(insert_stmt)
+    obj = result.fetchone()
+
+
+    # Store the inserted row if a key is provided
+    if key:
+        store.put(key, obj)
+
+    return obj
+
+
+async def load_core(metadata, connection, fixture_text, loader=None):
+    """
+    Load data from YAML into the database using SQLAlchemy Core.
+
+    :param metadata: SQLAlchemy MetaData object.
+    :param connection: SQLAlchemy connection object.
+    :param fixture_text: YAML string with data to load.
+    :param loader: YAML loader (optional).
+    """
+    if loader is None:
+        loader = yaml.FullLoader
+    data = yaml.load(fixture_text, Loader=loader)
+
+    if not isinstance(data, list):
+        raise ValueError('Top level YAML should be a sequence (list).')
+
+    # Reflect the tables from the metadata
+    tables = {table.name: table for table in metadata.sorted_tables}
+
+    store = Store()
+
+    # Iterate through the YAML data
+    for model_entry in data:
+        if len(model_entry) != 1:
+            raise ValueError('Each YAML entry must contain only one table.')
+
+        table_name, instances = model_entry.popitem()
+
+        if instances is None:
+            continue
+
+        if not isinstance(instances, list):
+            raise ValueError(f'`{table_name}` must contain a sequence (list).')
+
+        for fields in instances:
+            key = fields.pop('__key__', None)
+            await _insert_row(tables, connection, store, table_name, key, fields)
+
+    # Commit the transaction
+    # await connection.commit()
+
     return store
