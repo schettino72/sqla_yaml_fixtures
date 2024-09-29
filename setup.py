0166d22fb2c3ccc2fd20d8ccd50ad341121fbc08
Eduardo Schettino <schettino72@gmail.com>
2024-09-29 17:47:13 +0700

    setup.py, dont check for pip version.

--- setup.py
+++ setup.py
@@ -1,30 +1,5 @@
-from __future__ import print_function
 from setuptools import setup
 import sys
-
-# Assume pip is new enough
-pip_message = None
-try:
-    import pip
-    pip_version = tuple([int(x) for x in pip.__version__.split('.')[:3]])
-    if pip_version < (9, 0, 1) :
-        pip_message = 'Your pip version is out of date, please install pip >= 9.0.1. '\
-        'pip {} detected.'.format(pip.__version__)
-
-except Exception:
-    pip_message = 'This may be due to an out of date pip. Make sure you have pip >= 9.0.1.'
-
-
-if pip_message:
-    # Some error from pip version check
-    error = """
-    Python {py} detected.
-    Exiting setup:
-    {pip}
-    """.format(py=sys.version_info, pip=pip_message )
-
-    print(error, file=sys.stderr)
-    sys.exit(1)
 
 
 def readme():
